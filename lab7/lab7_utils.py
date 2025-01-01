[lab7] correct normalization problem

lab7/lab7_utils.py
@@ -85,9 +85,17 @@ def binarize(X_train, X_test):
 
 def flatten_image(X_train, X_test):
     X_train = X_train.reshape((X_train.shape[0], 784))
+    X_train.astype(np.uint8)
+    
     X_test = X_test.reshape((X_test.shape[0], 784))
+    X_test.astype(np.uint8)
     
     return X_train, X_test
+
+def flatten_and_normalize_image(X_train, X_test):
+    X_train , X_test = flatten_image(X_train, X_test)
+
+    return X_train / 255, X_test / 255
 
 def extend_images_to_4d(X_train, X_test):
   X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)) / 255.
@@ -118,7 +126,7 @@ def get_dataset_for_ae(dataset_name, with_val):
 
     X_train, X_test, y_train, y_test = fetch_fun(dataset_name)
 
-    X_train, X_test = flatten_image(X_train, X_test)
+    X_train, X_test = flatten_and_normalize_image(X_train, X_test)
 
     if with_val:
         X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=1/(1+6), random_state=seed)